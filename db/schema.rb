76ddb69e73b2b5579cc9a0a4e8a53b167689209a
sr-bauducco <israel.telesbandeira@gmail.com>
2025-02-02 16:14:41 -0300

    feito a implementação mas ainda não foi testada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_02_190411) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_02_191043) do
   create_table "formularios", force: :cascade do |t|
     t.string "tipo"
     t.integer "turma_id", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_02_190411) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "enrollment"
+    t.index ["enrollment"], name: "index_users_on_enrollment", unique: true
   end
 
   add_foreign_key "formularios", "turmas"
